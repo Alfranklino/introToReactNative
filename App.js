84d311de90729981a33fbed2700b89e5d0a39f5b
Alfranklino Computers <alfranklino@Alfranklinos-Mac.local>
2019-08-14 16:14:09 -0400

    Chore: Initial commit.

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Text}>
+        Here is my very first App Made with Love and the help of my friend... JD
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  Text: {
+    textAlign: "center",
+    color: "#f00"
+  }
+});
